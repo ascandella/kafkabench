ea063ca762938bbea474ad534e73c38af23f8b88
Aiden Scandella <aiden@uber.com>
2013-11-18 12:06:37 -0800

    Add topic listing

--- kafkabench.go
+++ kafkabench.go
@@ -3,17 +3,36 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/Shopify/sarama"
+	"log"
+	"time"
 )
 
 const (
 	defaultBrokerAddress = "127.0.0.1:9092"
 	defaultTopicName     = "benchmark"
+	clientId             = string(iota)
 )
 
 var (
 	brokerAddress = flag.String("address", defaultBrokerAddress, "broker address to connect to")
 	topicName     = flag.String("topic", defaultTopicName, "kafka topic to use")
+	clientConfig  = sarama.ClientConfig{
+		WaitForElection: time.Second,
+	}
 )
+
+func getClient(addr string) (client *sarama.Client) {
+	addrs := make([]string, 1)
+	addrs[0] = addr
+	client, err := sarama.NewClient(clientId, addrs, &clientConfig)
+
+	log.Print("Connecting to brokers: ", addrs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
 
 func usage() {
 	flag.Usage()
@@ -24,8 +43,14 @@ func main() {
 	flag.Parse()
 	if flag.NArg() == 0 {
 		usage()
-	} else {
-		fmt.Println("Nargs %d", flag.NArg())
-		fmt.Printf("%s", flag.Args())
+		return
+	}
+	client := getClient(*brokerAddress)
+	if flag.Args()[0] == "produce" {
+		if topics, err := client.Topics(); err != nil {
+			log.Fatal("Unable to list topics", err)
+		} else {
+			log.Println("Available topics", topics)
+		}
 	}
 }
